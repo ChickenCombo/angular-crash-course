67cf0b2439c7266203f445c16083bed2ad5b774b
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-06 13:33:08 +0800

    feat: toggle task reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -10,10 +10,15 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent {
   @Input() task: Task | null = null;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
   faTrash = faTrash;
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }
