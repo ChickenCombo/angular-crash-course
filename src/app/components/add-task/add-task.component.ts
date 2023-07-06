527d9d4f7db32124ed4fa5e1fcb1802c1788ef5e
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-06 14:07:40 +0800

    feat: add task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   task: string = '';
   day: string = '';
   reminder: boolean = false;
+
+  onSubmit() {
+    if (!this.task) {
+      alert('Please add a task!');
+      return;
+    }
+
+    const newTask: Task = {
+      text: this.task,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.task = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
