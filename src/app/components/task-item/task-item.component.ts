dbbacb7e70046f9c0332ba8f0530f4546be481a5
Lester Cuadra <lester.cuadra24@gmail.com>
2023-07-06 12:58:46 +0800

    feat: icons, task service, and fake rest server

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -8,4 +9,5 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent {
   @Input() task: Task | null = null;
+  faTrash = faTrash;
 }
